2cf78c75801825f362b02335672010c5dd66ffc3
hyunseok <prking94@naver.com>
2023-10-20 17:34:28 +0900

    2023-10-20 React Hooks를 이용 함수형 컴포넌트로 변경

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,46 +1,50 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Square from './Square'
 import "./Board.css"
-export default class Board extends Component {
+const Board = () => {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            squares : Array(9).fill(null) // fill 메서드는 배열의 시작 인덱스부터 끝 인덱스의 이전까지 정적인 값 하나로 채움. 
-        }
-    };
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         squares : Array(9).fill(null) // fill 메서드는 배열의 시작 인덱스부터 끝 인덱스의 이전까지 정적인 값 하나로 채움. 
+    //     }
+    // }; 
 
-    handleClick(i){
-      const squares = this.state.squares.slice();
-      squares[i] = 'X';
-      this.setState({squares : squares});
+    //Getter : 가져올 값 squares / Setter : 셋팅할 값 setSquares
+    const [squares, setSquares] = useState(Array(9).fill(null));
+
+    const handleClick = (i) => {
+      const newSquares = squares.slice();
+      newSquares[i] = 'X';
+      setSquares(newSquares);
     }
 
-    renderSquare(i) {
-        return <Square value={this.state.squares[i]}
-         onClick={() => this.handleClick(i) } />
+    const renderSquare = (i) => {
+        return <Square value={squares[i]}
+         onClick={() => handleClick(i) } />
     }
  
-  render() {
+  
     return (
       <div>
         <div className='status'>Next Player: X,O</div>
         <div className='board-row'>
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
+            {renderSquare(0)}
+            {renderSquare(1)}
+            {renderSquare(2)}
         </div>
         <div className='board-row'>
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
+            {renderSquare(3)}
+            {renderSquare(4)}
+            {renderSquare(5)}
         </div>
         <div className='board-row'>
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
+            {renderSquare(6)}
+            {renderSquare(7)}
+            {renderSquare(8)}
         </div>
       </div>
     )
   }
-}
+  export default Board;
+
