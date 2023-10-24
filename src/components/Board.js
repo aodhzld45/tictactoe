2c539b68c28b26376ebd507203e2d2e4fd1c24e0
hyunseok <prking94@naver.com>
2023-10-24 13:15:56 +0900

    2023-10-24 승자 결정하기 추가

--- src/components/Board.js
+++ src/components/Board.js
@@ -18,10 +18,48 @@ const Board = () => {
     const [squares, setSquares] = useState(Array(9).fill(null));
     const [XisNext, setXisNext] = useState(true);
 
-    const status = `Next Player ${XisNext ? 'X' : 'O'}`
+
+    // 승부를 결정하는걸 계산하는 함수
+    // X, O 한줄(빙고)로 같으면 되기에, lines 배열에 경우의 수를 나열
+    // for문 에서 만약 한줄로 같은 줄이 나오면 한 줄로 나열된 X 혹은 O를 return
+    /*
+        ex ['x','x','x',
+            'null','x','null',
+            'null','null','null']
+     */
+
+    const calculateWinner = (squares) => {
+      const lines = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+      ];
+  
+    for (let i = 0; i < lines.length; i++) {
+      const [a,b,c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+
+    return null;
+  }
+
+  // 승자를 표시해주는 함수
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+      status = 'Winner:'+ winner;
+    } else {
+      status = `Next Player ${XisNext? 'X' : 'O'}`;
+    }
+
     // const [number, setNumber] = useState(0);
-
-
 
     const handleClick = (i) => {
       const newSquares = squares.slice();
